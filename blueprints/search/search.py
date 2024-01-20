ed2d4cd9d32ccd2435d93fc484734fdb300d86e9
Isak-Landin <utbisalan@gmail.com>
2024-01-20 13:04:18 +0100

    Restructured search.py's json delivery system for better app compability

--- blueprints/search/search.py
+++ blueprints/search/search.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, jsonify, Blueprint
 from flask import current_app
 
+import pymongo
+
 import datetime
+
+from management_helpers.format_json_for_frontend import format_results
 
 search_bp = Blueprint('search_bp', __name__)
 
@@ -19,24 +23,8 @@ def perform_search(searchterm):
     regex_pattern = f'.*{searchterm}.*'
     regex = {'$regex': regex_pattern, '$options': 'i'}
 
-    result_cursor = current_app.db.entries.find({'content': regex})
+    result_cursor = current_app.db.entries.find({'content': regex}).sort("date", pymongo.DESCENDING)
     results = list(result_cursor)
 
-    formatted_results = [
-        {
-            'title': result.get('content', '') + '...' if len(result.get('content', '')) > 30 else result.get('content', ''),
-            'date': result.get('date', ''),  # Assuming 'date' is already in 'YYYY-MM-DD' format
-            'content': result.get('content', ''),
-            'formattedDate': format_date(result.get('date', ''))
-        }
-        for result in results
-    ]
+    formatted_results = format_results(results)
     return formatted_results
-
-
-def format_date(date_str):
-    if date_str is None:
-        return 'Unspecified'
-    # Convert 'YYYY-MM-DD' to a more readable format like 'Jan 01'
-    formatted_date = datetime.datetime.strptime(date_str, '%Y-%m-%d').strftime("%b %d")
-    return formatted_date
