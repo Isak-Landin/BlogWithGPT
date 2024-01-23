466b678fe86d6719456b5a2b56be3fa8de18db77
Isak-Landin <utbisalan@gmail.com>
2024-01-23 08:38:27 +0100

    Changing how notes are built and rendered

---
+++ models/notes.py
@@ -0,0 +1,10 @@
+import mongoengine as me
+import datetime
+
+
+class Note(me.Document):
+    meta = {'collection': 'entries'}
+    content = me.StringField(max_length=1000, required=True)
+    created_at = me.DateTimeField(required=True, default=datetime.datetime.now().strftime('%Y-%m-%d'))
+    updated_at = me.DateTimeField(required=True, default=datetime.datetime.now().strftime('%Y-%m-%d'))
+    user_id = me.StringField(max_length=100, required=True)
