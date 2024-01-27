f1a9d79c3ee4b427f11b60ed30516f6b5efd2b5c
Isak-Landin <utbisalan@gmail.com>
2024-01-28 00:53:14 +0100

    Replace article/s creation with imported module

--- static/js/search_entries.js
+++ static/js/search_entries.js
@@ -1,3 +1,6 @@
+import {renderEntry} from './renderEntry.js';
+
+
 document.addEventListener(
     'DOMContentLoaded', () => {
         const search_field = document.getElementById('search_field');
@@ -81,49 +84,7 @@ function renderEntries(entries) {
     container.innerHTML = ''; // Clear existing entries
 
     entries.forEach(entry => {
-        const article = document.createElement('article');
-        article.className = 'entry';
-        article.id = entry._id;
-
-        const header = document.createElement('div');
-        header.className = 'entry__header';
-
-        const title = document.createElement('h2');
-        title.className = 'entry__title';
-        title.textContent = entry.title;
-
-        const time = document.createElement('time');
-        time.className = 'entry__date';
-        time.setAttribute('datetime', entry.date);
-        time.textContent = '· ' + entry.formattedDate;
-
-        header.appendChild(title);
-        header.appendChild(time);
-
-        const content = document.createElement('p');
-        content.className = 'entry__content';
-        content.textContent = entry.content;
-
-        const footer = document.createElement('div');
-        footer.className = 'entry__footer';
-
-        const editLink = document.createElement('a');
-        editLink.href = '#';
-        editLink.className = 'entry__footer-edit';
-        editLink.textContent = '✎';
-
-        const removeLink = document.createElement('a');
-        removeLink.href = '#';
-        removeLink.className = 'entry__footer-remove';
-        removeLink.textContent = '✕';
-
-        footer.appendChild(editLink);
-        footer.appendChild(removeLink);
-
-        article.appendChild(header);
-        article.appendChild(content);
-        article.appendChild(footer);
-
-        container.appendChild(article);
+        const noteObject = renderEntry(entry);
+        container.appendChild(noteObject);
     });
 }
