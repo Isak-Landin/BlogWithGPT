b15002c946fd954987ec104bb631c771eb2aef0f
Isak-Landin <utbisalan@gmail.com>
2024-02-03 15:32:49 +0100

    Reformatting data

--- blueprints/home/views.py
+++ blueprints/home/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, current_app, render_template, jsonify
 import pymongo
 import datetime
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
+from models.notes import Note
 from management_helpers.format_json_for_frontend import format_results
 from . import home_bp
 from blueprints.edit.forms import NoteForm
@@ -13,15 +14,15 @@ from blueprints.edit.forms import NoteForm
 @login_required
 def home():
     form = NoteForm()
-
+    print(current_user.pk)
     entries_with_date = [
         (
-            entry["content"],
-            entry["updated_at"],
-            entry["updated_at"].strftime("%b %d"),
-            str(entry["_id"])
+            entry.content,
+            entry.updated_at,
+            entry.updated_at.strftime("%b %d"),
+            str(entry.pk)
         )
-        for entry in current_app.db.entries.find({}).sort("updated_at", pymongo.DESCENDING)
+        for entry in Note.objects(user_id=str(current_user.pk), is_deleted=False).order_by('-updated_at')
     ]
 
     return render_template("home.html", entries=entries_with_date, form=form)
@@ -30,7 +31,7 @@ def home():
 @home_bp.route("/load-default-notes", methods=['GET',])
 @login_required
 def load_notes():
-    entries = current_app.db.entries.find({}).sort("date", pymongo.DESCENDING)
+    entries = Note.objects(user_id=str(current_user.pk), is_deleted=False).order_by('-updated_at')
 
     # Create formatted results
     formatted_results = format_results(entries)
