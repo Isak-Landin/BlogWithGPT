e3bd45cfc11791641b0ae6fcc4b7334aee6cefe9
Isak-Landin <utbisalan@gmail.com>
2024-02-03 15:33:39 +0100

    Add deleted columns to collection

--- models/notes.py
+++ models/notes.py
@@ -8,3 +8,5 @@ class Note(me.Document):
     created_at = me.DateTimeField(required=True, default=datetime.datetime.now())
     updated_at = me.DateTimeField(required=True, default=datetime.datetime.now())
     user_id = me.StringField(max_length=100, required=True)
+    is_deleted = me.BooleanField(required=False, default=False)
+    deleted_at = me.DateTimeField(required=False, default=None)
