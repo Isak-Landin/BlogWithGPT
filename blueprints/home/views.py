f245daf04c313ae6b33b836de03ae079e7284cfe
Isak-Landin <utbisalan@gmail.com>
2024-02-04 14:30:56 +0100

    Moved loading singular note to home from edit
    Still remains as a failsafe in edit

--- blueprints/home/views.py
+++ blueprints/home/views.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, request, current_app, render_template, jsonify
+from flask import (
+    Blueprint, request, current_app, render_template, jsonify,
+    abort
+)
 import pymongo
 import datetime
 from flask_login import login_required, current_user
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 
 
 from models.notes import Note
-from management_helpers.format_json_for_frontend import format_results
+from management_helpers.format_json_for_frontend import format_results, format_date
 from . import home_bp
 from blueprints.edit.forms import NoteForm
 
@@ -37,3 +42,54 @@ def load_notes():
     formatted_results = format_results(entries)
 
     return jsonify(formatted_results)
+
+
+@home_bp.route("/load-note/<mongo_id>", methods=['GET',])
+@login_required
+def load_note(mongo_id):
+    _id = ObjectId(mongo_id)
+    if not is_valid_object_id(mongo_id) or not does_entry_exist(mongo_id):
+        abort(404)
+    note = Note.objects(user_id=str(current_user.pk), pk=_id).first()
+
+    if not note:
+        abort(500)
+
+    def format_response_new_note(new_note_from_entry):
+        return {
+            'title': new_note_from_entry.content[:30] + '...' if len(note["content"]) > 30 else note["content"],
+            'date': new_note_from_entry.updated_at,
+            'content': new_note_from_entry.content,
+            'formattedDate': format_date(new_note_from_entry.updated_at),
+            '_id': str(new_note_from_entry.pk)
+        }
+
+    try:
+        formatted_note = format_response_new_note(note)
+        message = 'Success'
+        data = {
+            'note': formatted_note,
+            'message': message,
+        }
+    except Exception:
+        message = 'Error formatting new note'
+        data = {
+            'message': message
+        }
+
+    response_ = data
+
+    return response_
+
+
+def is_valid_object_id(_id):
+    try:
+        ObjectId(_id)
+        return True
+    except InvalidId:
+        return False
+
+
+def does_entry_exist(mongo_id):
+    entry = current_app.db.entries.find_one({"_id": ObjectId(mongo_id)})
+    return entry is not None
