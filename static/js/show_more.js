daeb692bb754098da5a4b84e6e0721999e6f44a5
Isak-Landin <utbisalan@gmail.com>
2024-02-12 08:34:29 +0100

    Apply show more to route-rendered template

--- static/js/show_more.js
+++ static/js/show_more.js
@@ -1,4 +1,5 @@
 import {renderShowMoreModal} from '/static/js/render.js';
+import {renderShowMore} from '/static/js/render.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,6 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
         all_show_mores.forEach(show_more => {
             show_more.addEventListener('click', show_more);
         });
+
+        let all_entries_shown = document.querySelectorAll('.entry');
+        all_entries_shown.forEach(entry => {
+            const content = entry.querySelector('.entry__content');
+            if (content.scrollHeight > content.clientHeight) {
+                const showMoreDiv = renderShowMore();
+
+                const footer = entry.querySelector('.entry__footer');
+                const removeLink = footer.querySelector('.entry__footer-remove');
+                footer.insertBefore(showMoreDiv, removeLink);
+                console.log('This is a confirmation that show more is needed');
+            }
+        });
+
     }
 );
 
