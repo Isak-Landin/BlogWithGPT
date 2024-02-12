5e6a7c0c5a9dcd2ea14e80dce91a5091f7ef0abd
Isak-Landin <utbisalan@gmail.com>
2024-02-12 10:36:23 +0100

    Needs further styling and functionality
    We need to change how the already existing event-listeners interact with what element.
    
    Currently the event-listeners on the clone will interact with the entry on the page rather than the cloned entry on the modal

--- static/js/show_more.js
+++ static/js/show_more.js
@@ -1,5 +1,7 @@
 import {renderShowMoreModal} from '/static/js/render.js';
 import {renderShowMore} from '/static/js/render.js';
+import {removeElement} from '/static/js/render.js';
+import {handleEscapeKeyDown} from '/static/js/render.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,9 +30,29 @@ document.addEventListener('DOMContentLoaded', () => {
 // <a href="#" class="entry__footer-show-more">Show more</a>
 export function show_more(event) {
     const target = event.target;
-    const entry = target.parentNode.parentNode;
-    const content = entry.querySelector('.entry__content');
+    const entry = target.parentNode.parentNode.parentNode;
 
-    const modal_backdrop = renderShowMoreModal();
+    const entry_clone = entry.cloneNode(true);
+    entry_clone.id = 'entryClone';
+    entry_clone.setAttribute('original-id', entry.id);
 
+    const modal_backdrop = renderShowMoreModal(event);
+    modal_backdrop.appendChild(entry_clone);
+
+    if (document.querySelector('.show-more__modal-backdrop') != null) {
+        removeElement(document.querySelector('.show-more__modal-backdrop'));
+    }
+
+    document.body.appendChild(modal_backdrop);
+}
+
+export function cancel_show_more(event) {
+    console.log('cancel_show_more just got triggered');
+    event.preventDefault();
+    var all_existing_modal_backdrops = document.querySelectorAll('.show-more__modal-backdrop');
+    all_existing_modal_backdrops.forEach(modal_backdrop => {
+        removeElement(modal_backdrop);
+    });
+
+    document.removeEventListener('keydown', handleEscapeKeyDown);
 }
